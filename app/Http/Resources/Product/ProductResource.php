f619ac7c435deda4d23bc3bdebc51ce001badfac
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2020-08-21 14:02:48 -0700

    Added resource for transformation of Products and reviews

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -14,6 +14,18 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name'  => $this->name,
+            'price'  => $this->price,
+            'stock'  => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount'  => $this->discount,
+            'totalPrice'  => round($this->price - ($this->discount / 100 * $this->price), ),
+            'description'  => $this->description,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('rating')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ],
+        ];
     }
 }
