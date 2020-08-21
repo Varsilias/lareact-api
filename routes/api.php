8216f26926394a9a93eabbfafaa27b42e202fc91
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2020-08-21 07:39:19 -0700

    Added API resource route in the route/api.php file

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Product API route
+Route::apiResource('products', 'ProductController');
+
+// Review API route
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{product}/reviews', 'ReviewController');
 });
