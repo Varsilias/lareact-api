a211ab0b3bc8d9c9011b13211974c7cff788a8a9
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2020-08-21 05:29:59 -0700

    Initial commit

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Review;
+use APP\Model\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Review::class, function (Faker $faker) {
+    return [
+        'product_id' => function(){
+            return Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'rating' => $faker->numberBetween(0, 5)
+    ];
+});
